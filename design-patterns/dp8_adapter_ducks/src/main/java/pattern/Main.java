6b0f022d21dbc7b69eb3dd3bd4f4fff41f2e43df
abondada <abondada@microsoft.com>
2025-05-23 10:51:41 +0530

    refactored to add single class with main function and the corresponding pom changes to build the correct jar

--- design-patterns/dp8_adapter_ducks/src/main/java/pattern/Main.java
+++ design-patterns/dp8_adapter_ducks/src/main/java/pattern/Main.java
@@ -4,15 +4,16 @@ import pattern.Drone.Drone;
 import pattern.Drone.DroneAdapter;
 import pattern.Drone.Quadcopter.Quadcopter;
 import pattern.Duck.Duck;
+import pattern.Duck.DuckAdapter;
 import pattern.Duck.MallardDuck.MallardDuck;
 import pattern.Turkey.Turkey;
 import pattern.Turkey.TurkeyAdapter;
 import pattern.Turkey.WildTurkey.WildTurkey;
 
-public class DuckTestDrive {
+public class Main {
 	public static void main(String[] args) {
+		// Adapter : Turkey to Duck
 		Duck duck = new MallardDuck();
-
 		Turkey turkey = new WildTurkey();
 		Duck turkeyAdapter = new TurkeyAdapter(turkey);
 
@@ -26,11 +27,21 @@ public class DuckTestDrive {
 		System.out.println("\nThe TurkeyAdapter says...");
 		testDuck(turkeyAdapter);
 		
-		// Challenge
+        // Adapter : Drone to Duck
 		System.out.println("\nThe DroneAdapter says...");
 		Drone drone = new Quadcopter();
 		Duck droneAdapter = new DroneAdapter(drone);
 		testDuck(droneAdapter);
+
+		// Adapter : Duck to Turkey 
+		MallardDuck duck2 = new MallardDuck();
+		Turkey duckAdapter = new DuckAdapter(duck2);
+
+		for(int i=0;i<10;i++) {
+			System.out.println("The DuckAdapter says...");
+			duckAdapter.gobble();
+			duckAdapter.fly();
+		}
 	}
 
 	static void testDuck(Duck duck) {
