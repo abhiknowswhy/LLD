1534c305886a09aff0babdd6b13457bd2d98a18c
abondada <abondada@microsoft.com>
2025-05-25 00:53:10 +0530

    minor warning fix

--- design-patterns/dp12_composite_mergedcafe/src/test/java/WaitressTest.java
+++ design-patterns/dp12_composite_mergedcafe/src/test/java/WaitressTest.java
@@ -3,7 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.Arrays;
 import java.util.Iterator;
 
 import org.junit.jupiter.api.AfterEach;
